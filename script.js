7d7786cadee85ff0209fb9e58670af04bab0e030
jillwilliams <codeGeek.03.14@gmail.com>
2020-09-11 18:16:12 -0700

    Final Draft

---
+++ script.js
@@ -0,0 +1,27 @@
+const myProblem = () => {
+    return "Try Again!";
+};
+const myCalc = () => {
+    document.getElementById("demo").innerHTML = myProblem();
+}
+
+const myProblem2 = () => {
+    return "Good Job!";
+};
+const myCalc2 = () => {
+    document.getElementById("demo").innerHTML = myProblem2();
+}
+
+const myProblem3 = () => {
+    return "Try Again!";
+};
+const myCalc3 = () => {
+    document.getElementById("demo2").innerHTML = myProblem3();
+}
+
+const myProblem4 = () => {
+    return "Good Job!";
+};
+const myCalc4 = () => {
+    document.getElementById("demo2").innerHTML = myProblem4();
+}
